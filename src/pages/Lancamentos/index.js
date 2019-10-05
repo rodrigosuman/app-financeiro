13e989098710fe92dfa25da0b6f8c54c3a297774
rodrigosuman <rodrigosumanzz@gmail.com>
2019-10-05 01:48:57 -0300

    implementado layout para busca por mes e resumo do mes na tela de transações

--- src/pages/Lancamentos/index.js
+++ src/pages/Lancamentos/index.js
@@ -1,14 +1,21 @@
 import React, {useState, useEffect} from 'react';
-
 import {RefreshControl} from 'react-native';
 import {FloatingAction} from 'react-native-floating-action';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, List, TextFeedback} from './styles';
-
-import {getObjects, deleteAll} from '~/services/realm';
+import {getObjects} from '~/services/realm';
 
 import Lancamento from '~/pages/Lancamento';
+
+import {
+  Container,
+  List,
+  Header,
+  Mes,
+  ResumoDoMes,
+  Content,
+  TextFeedback,
+} from './styles';
 
 export default function Lancamentos({navigation}) {
   const [fetching, setfetching] = useState(false);
@@ -36,58 +43,83 @@ export default function Lancamentos({navigation}) {
   }
 
   return (
-    <Container>
-      <List
-        onEndReachedThreshold={0.3}
-        onEndReached={() => fetchingList()}
-        refreshControl={
-          <RefreshControl
-            refreshing={fetching}
-            onRefresh={() => fetchingList()}
-          />
-        }
-        keyboardShouldPersistTaps="handle"
-        data={lancamentos}
-        keyExtractor={item => String(item.id)}
-        renderItem={({item}) => (
-          <Lancamento
-            key={item.id}
-            data={item}
-            callBack={fetchingList}
-            navigation={navigation}
-          />
-        )}
-      />
+    <>
+      <Header>
+        <Icon name="chevron-left" color="#fff" size={23} />
+        <Mes>Setembro, 2019</Mes>
+        <Icon name="chevron-right" color="#fff" size={23} />
+      </Header>
 
-      <FloatingAction
-        actions={actions}
-        color="#F7385E"
-        onPressItem={name => {
-          switch (name) {
-            case 'bt_receita':
-              navigate('NovoLancamento', {
-                receita: true,
-                callBack: fetchingList,
-                insert: true,
-                lancamento: lancamento,
-              });
-              break;
+      <ResumoDoMes>
+        <Content>
+          <Icon name="arrow-downward" color="#FFB800" size={23} />
+          <TextFeedback>R$ 0.00</TextFeedback>
+        </Content>
 
-            case 'bt_despesa':
-              navigate('NovoLancamento', {
-                receita: false,
-                callBack: fetchingList,
-                insert: true,
-                lancamento: lancamento,
-              });
-              break;
+        <Content>
+          <Icon name="arrow-upward" color="#00FFFF" size={23} />
+          <TextFeedback>R$ 3900.00</TextFeedback>
+        </Content>
 
-            default:
-              break;
+        <Content>
+          <Icon name="chevron-right" color="#2E008B" size={23} />
+          <TextFeedback>R$ 3900.00</TextFeedback>
+        </Content>
+      </ResumoDoMes>
+
+      <Container>
+        <List
+          onEndReachedThreshold={0.3}
+          onEndReached={() => fetchingList()}
+          refreshControl={
+            <RefreshControl
+              refreshing={fetching}
+              onRefresh={() => fetchingList()}
+            />
           }
-        }}
-      />
-    </Container>
+          keyboardShouldPersistTaps="handle"
+          data={lancamentos}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => (
+            <Lancamento
+              key={item.id}
+              data={item}
+              callBack={fetchingList}
+              navigation={navigation}
+            />
+          )}
+        />
+
+        <FloatingAction
+          actions={actions}
+          color="#F7385E"
+          onPressItem={name => {
+            switch (name) {
+              case 'bt_receita':
+                navigate('NovoLancamento', {
+                  receita: true,
+                  callBack: fetchingList,
+                  insert: true,
+                  lancamento: lancamento,
+                });
+                break;
+
+              case 'bt_despesa':
+                navigate('NovoLancamento', {
+                  receita: false,
+                  callBack: fetchingList,
+                  insert: true,
+                  lancamento: lancamento,
+                });
+                break;
+
+              default:
+                break;
+            }
+          }}
+        />
+      </Container>
+    </>
   );
 }
 
--- src/pages/Lancamentos/styles.js
+++ src/pages/Lancamentos/styles.js
@@ -9,6 +9,34 @@ export const Container = styled.View`
   padding: 12px;
 `;
 
+export const Header = styled.View`
+  height: 50px;
+  padding: 15px;
+  padding-left: 60px;
+  padding-right: 60px;
+  background-color: #1abc9c;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Content = styled.View`
+  flex-direction: row;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const ResumoDoMes = styled.View.attrs([boxShadow])`
+  height: 15px;
+  padding: 25px;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
 export const ViewSearch = styled.View.attrs([boxShadow])`
   flex-direction: column;
   width: 100%;
@@ -33,5 +61,10 @@ export const List = styled.FlatList.attrs({
 
 export const TextFeedback = styled.Text`
   color: #666;
+  text-align: center;
+`;
+
+export const Mes = styled.Text`
+  color: #fff;
   text-align: center;
 `;
